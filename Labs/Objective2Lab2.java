d410284c9706dc35055943577ab66746269116c1
Ryan Majors <growbox.ryan@yahoo.com>
2021-09-02 17:23:48 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ryan Majors";
+    int age = 34;
+    String state = "Colorado";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
