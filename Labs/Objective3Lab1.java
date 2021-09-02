ed392428497c6483ff30bdc4262846e1a902076f
Ryan Majors <growbox.ryan@yahoo.com>
2021-09-02 17:50:41 -0600

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,12 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average, sum;
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+    sum = num1 + num2 + num3;
+    average = sum / 3;
+    System.out.println(average);
+
+  }
+}
