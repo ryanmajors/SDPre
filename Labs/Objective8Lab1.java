fbca8cf6b03df7e17f1cd5e6f154c53bceaff963
Ryan Majors <growbox.ryan@yahoo.com>
2021-09-05 20:50:18 -0600

    objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int x = 1; x <= 10; x++) {
+      System.out.println(x);
+    }
+    for (int y = 10; y >= 1; y--) {
+      System.out.println(y);
+    }
+
+  }
+}
